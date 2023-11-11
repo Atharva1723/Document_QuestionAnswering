ad5e668328f298fe836d0863a7f8a652ed4b6bec
Atharva Kulkarni <atharvakulkarni172003@gmail.com>
2023-11-11 22:20:51 +0530

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,10 @@
+# AUTHOR : TECHMAHINDRA MAKERS LAB #
+
+# Import the AppConfig class from django.apps
+from django.apps import AppConfig
+
+# Create a custom AppConfig for the 'bot_registration' app
+class BotRegistrationConfig(AppConfig):
+    # Set the name of the app
+    name = 'bot_registration'
+
